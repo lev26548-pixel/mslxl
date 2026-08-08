5e509e1cdf9e25f80546464b86a11671c0b22af1
lelele lo iweh:) <lev26548@gmail.com>
2026-08-08 05:36:48 +0500

    Download mszx.py from GitHub repository

---
+++ dowload-or_github.py
@@ -0,0 +1,1 @@
+import urllib.request; urllib.request.urlretrieve('https://raw.githubusercontent.com/lev26548-pixel/mszx/refs/heads/main/mszx.py', 'mszx.py'); print(':) библиотека mszx.py успешно загружена!')
