d805729f87fba3abff8c144946c490b0092b6cee
lelele lo iweh:) <lev26548@gmail.com>
2026-08-08 05:37:11 +0500

    Fix print statement in dowload.py

--- dowload.py
+++ dowload.py
@@ -1,1 +1,1 @@
-import urllib.request; urllib.request.urlretrieve('https://szx.pythonanywhere.com/mszx.py', 'mszx.py'); print('[+] библиотека mszx.py успешно загружена!')
+import urllib.request; urllib.request.urlretrieve('https://szx.pythonanywhere.com/mszx.py', 'mszx.py'); print(':) библиотека mszx.py успешно загружена!')
